626c45e93da28a0e82388faf8a45c26281943878
Snazzah <suggesttosnazzy@gmail.com>
2021-09-29 00:04:02 -0500

    feat: add `/bot info`

--- src/commands/bot.ts
+++ src/commands/bot.ts
@@ -1,4 +1,11 @@
-import { SlashCommand, CommandOptionType, SlashCreator, CommandContext } from 'slash-create';
+import {
+  SlashCommand,
+  CommandOptionType,
+  ComponentType,
+  ButtonStyle,
+  SlashCreator,
+  CommandContext
+} from 'slash-create';
 import { devGuild, quickLinkButton } from '../util';
 
 export default class BotCommand extends SlashCommand {
@@ -17,6 +24,11 @@ export default class BotCommand extends SlashCommand {
           type: CommandOptionType.SUB_COMMAND,
           name: 'github',
           description: 'Get the GitHub repository link for ducksearch.'
+        },
+        {
+          type: CommandOptionType.SUB_COMMAND,
+          name: 'info',
+          description: 'Get information on ducksearch.'
         }
       ]
     });
@@ -47,6 +59,36 @@ export default class BotCommand extends SlashCommand {
             },
             false
           )
+        ]
+      };
+    else if (ctx.subcommands[0] === 'info')
+      return {
+        content:
+          '**ducksearch** is a bot that searches from DuckDuckGo with Discord slash commands with some other API commands included.',
+        components: [
+          {
+            type: ComponentType.ACTION_ROW,
+            components: [
+              {
+                type: ComponentType.BUTTON,
+                style: ButtonStyle.LINK,
+                label: 'Invite ducksearch',
+                url: 'https://discord.com/oauth2/authorize?client_id=886879779318530058&permissions=0&scope=applications.commands%20bot'
+              },
+              {
+                type: ComponentType.BUTTON,
+                style: ButtonStyle.LINK,
+                label: 'View GitHub Repository',
+                url: 'https://github.com/Snazzah/ducksearch'
+              },
+              {
+                type: ComponentType.BUTTON,
+                style: ButtonStyle.LINK,
+                label: 'Sponsor',
+                url: 'https://github.com/sponsors/Snazzah'
+              }
+            ]
+          }
         ]
       };
 
